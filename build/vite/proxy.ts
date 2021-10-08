ba4dfdf07ba3113b5eecd8c3b6898c1a82d7ebe2
HADES <1005710179@qq.com>
2021-10-08 16:23:56 +0800

    *

--- build/vite/proxy.ts
+++ build/vite/proxy.ts
@@ -16,10 +16,10 @@
   * @param list
   */
  export function createProxy(list: ProxyList = []) {
+
    const ret: ProxyTargetList = {};
    for (const [prefix, target] of list) {
      const isHttps = httpsRE.test(target);
- 
      // https://github.com/http-party/node-http-proxy#options
      ret[prefix] = {
        target: target,
