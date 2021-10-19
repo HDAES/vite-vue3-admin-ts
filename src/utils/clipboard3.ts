83a7296eb8c7a0c8455e97c3e2f8ff24eb4b3124
HADES <1005710179@qq.com>
2021-10-19 17:47:14 +0800

    格式整理

--- src/utils/clipboard3.ts
+++ src/utils/clipboard3.ts
@@ -1,38 +1,38 @@
 /*
- * @Descripttion: 
+ * @Descripttion: 粘贴板
  * @Author: Hades
  * @Date: 2021-10-19 14:37:42
  */
 import Clipboard from 'clipboard'
 
-export default (opts) =>{
-    const appendToBody = opts?.appendToBody === undefined ? true : opts.appendToBody
+export default (opts) => {
+  const appendToBody = opts?.appendToBody === undefined ? true : opts.appendToBody
 
-    return {
-        toClipboard(text,container){
-            return new Promise((resolve, reject) => {
-                const fakeEl = document.createElement('button')
-                // setup a new Clipboard.js
-                const clipboard = new Clipboard(fakeEl, {
-                  text: () => text,
-                  action: () => 'copy',
-                  container: container !== undefined ? container : document.body
-                })
-                clipboard.on('success', (e) => {
-                  clipboard.destroy()
-                  resolve(e)
-                })
-                clipboard.on('error', (e) => {
-                  clipboard.destroy()
-                  reject(e)
-                })
-                // appendToBody fixes IE
-                if (appendToBody) document.body.appendChild(fakeEl)
-                // simulate click
-                fakeEl.click()
-                // remove from body if appended
-                if (appendToBody) document.body.removeChild(fakeEl)
-            })
-        }
+  return {
+    toClipboard(text, container) {
+      return new Promise((resolve, reject) => {
+        const fakeEl = document.createElement('button')
+        // setup a new Clipboard.js
+        const clipboard = new Clipboard(fakeEl, {
+          text: () => text,
+          action: () => 'copy',
+          container: container !== undefined ? container : document.body
+        })
+        clipboard.on('success', (e) => {
+          clipboard.destroy()
+          resolve(e)
+        })
+        clipboard.on('error', (e) => {
+          clipboard.destroy()
+          reject(e)
+        })
+        // appendToBody fixes IE
+        if (appendToBody) document.body.appendChild(fakeEl)
+        // simulate click
+        fakeEl.click()
+        // remove from body if appended
+        if (appendToBody) document.body.removeChild(fakeEl)
+      })
     }
+  }
 }
