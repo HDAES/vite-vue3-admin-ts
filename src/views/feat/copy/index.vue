4724235560f0892d30c3b5f1b2136502220759bf
HADES <1005710179@qq.com>
2021-10-19 14:51:13 +0800

    粘贴板

---
+++ src/views/feat/copy/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <el-card header="粘贴板">
+           <el-input placeholder="请输入内容" v-model="text">
+                <template #append>
+                    <el-button icon="el-icon-s-claim" @click="handleCopy"></el-button>
+                </template>
+            </el-input>
+      </el-card>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue' 
+import  useClipboard from '@/utils/clipboard3'
+import { ElMessage } from 'element-plus'
+export default {
+    setup(){
+        const text = ref('')
+        const { toClipboard } = useClipboard()
+        const handleCopy = async () =>{
+            try{
+                await toClipboard(text.value)
+                ElMessage.success('复制成功！')
+            }catch(e){
+                ElMessage.error(e)
+            }
+        }
+        return {
+            text,
+            handleCopy
+        }
+    }
+}
+</script>
